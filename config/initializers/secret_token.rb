c168e557d5f5eb3d7c574bf3ff506e46c2bf96ac
Grayson Carroll <carrollgt91@gmail.com>
2012-12-21 23:23:08 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoliticalIdTest::Application.config.secret_token = '151538bc48c408b8026b153f8bb3a0d541b33d528bef564038f2ae05a3dbd5f349c59ee9a24e48e2cea9ec5cb7323f8965fdf162f0abfba74621549c0bf44ab1'
