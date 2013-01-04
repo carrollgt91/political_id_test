3a48e743b91080f44287a3edfeba526031836af6
Grayson Carroll <carrollgt91@gmail.com>
2013-01-04 17:20:44 -0600

    Extends user model and tests to cover naiive email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
   validates :name, :email, presence: true
   validates :name, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX },
+  									uniqueness: { case_sensitive: false }
 end
