1b1eb1a46007f62bc328a7d2a81c2d84ebe2abcb
Grayson Carroll <carrollgt91@gmail.com>
2013-01-16 11:30:54 -0600

    Adds bootstrap styling and some minor changes to the html for the home page

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ PoliticalIdTest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'survey#home'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
