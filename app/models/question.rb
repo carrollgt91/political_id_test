9f5c298f6befdb8c01d1bb039089b3a268af44b3
Grayson Carroll <carrollgt91@gmail.com>
2013-01-23 10:30:11 -0600

    Adds proper linking for the questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -24,4 +24,12 @@ class Question < ActiveRecord::Base
     end
   end
   
+  def previous
+    Question.where(["id < ?", id]).last
+  end
+
+  def next
+    Question.where(["id > ?", id]).first
+  end
+
 end
