a9e53a2d1fd030d1af8dba59a312a1b06a960299
Grayson Carroll <carrollgt91@gmail.com>
2013-01-31 16:01:48 -0600

    Removes the survey controller/views and adds the foundational logic for keeping a running result of the users scores.

--- app/models/result.rb
+++ app/models/result.rb
@@ -6,11 +6,41 @@ class Result < ActiveRecord::Base
   belongs_to :user
 
   def calculate_score(response)
-  	case Question.find(response.question_id).q_type
-  	when 0 #economic
-		when 1 #social
-		when 2 #foreign_p
-  	end
+    binding.pry
+    if(response.id)
+      recalculate_score(response)
+    else
+      calculate_new_score(response)
+    end
   end
 
+
+  def recalculate_score(response)
+    old_response = response.user.responses(response.id)
+    question = Question.find(response.question_id)
+    sign = question.orientation ? 1 : -1
+
+    case Question.find(response.question_id).q_type
+    when 0 #economic
+      economic_score = economic_score - (sign*old_response.answer) + (sign*response.answer)
+    when 1 #social
+      social_score = social_score - (sign*old_response.answer) + (sign*response.answer)
+    when 2 #foreign_p
+      foreign_p_score = foreign_p_score - (sign*old_response.answer) + (sign*response.answer)
+    end
+  end
+
+  def calculate_new_score(response)
+    question = Question.find(response.question_id)
+    sign = question.orientation ? 1 : -1
+
+    case Question.find(response.question_id).q_type
+    when 0 #economic
+      economic_score = economic_score + (sign*response.answer)
+    when 1 #social
+      social_score = social_score + (sign*response.answer)
+    when 2 #foreign_p
+      foreign_p_score = foreign_p_score + (sign*response.answer)
+    end
+  end
 end
